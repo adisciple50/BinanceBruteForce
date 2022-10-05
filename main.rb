4bb1b91ad00091b4c5961e65ed5ba7367febda1a
Jason Crockett <JesusisLord33333@outlook.com>
2022-10-05 22:53:27 +0100

    final version

--- main.rb
+++ main.rb
@@ -17,7 +17,14 @@ def resulting_currency_pairs
   end
 end
 
-RESULTING_CURRENCY_PAIRS = resulting_currency_pairs
+def remove_resulting_currency(order)
+  order['symbol'].delete_prefix(RESULTING_CURRENCY).delete_suffix(RESULTING_CURRENCY)
+end
+
+RESULTING_CURRENCY_PAIRS = Parallel.map(RESULTING_CURRENCY_ORDERS,in_threads:RESULTING_CURRENCY_ORDERS.length) do |order|
+  # remove resulting currency from orders
+  remove_resulting_currency order
+end
 
 def sorted_orders(resulting_currency_orders)
   result = resulting_currency_orders.sort_by(){|order| order['askPrice'].to_f <=> order['askPrice'].to_f}
@@ -34,47 +41,64 @@ end
 
 RESULTING_CURRENCY_SORTED_ORDERS = sorted_orders(RESULTING_CURRENCY_ORDERS)
 
-TRADE1_SET = cheapest_pair RESULTING_CURRENCY_SORTED_ORDERS
-
+TRADE1_SET = RESULTING_CURRENCY_ORDERS
 
 
 TRADE2_SET = Parallel.map(RESULTING_CURRENCY_ORDERS,in_threads:RESULTING_CURRENCY_ORDERS.length) do |order|
   # trade1 is order
-  search = order['symbol'].delete_prefix(RESULTING_CURRENCY).delete_suffix(RESULTING_CURRENCY)
+  search = remove_resulting_currency(order)
   {order => ORDER_BOOK.select(){|trade| trade['symbol'].include? search }}
 end
 # see if any coin list
-def search_for_partial_matches(symbol,coin_list)
+def search_for_partial_matches(trade,order_array,resulting_pairs)
   begin
-    check = coin_list.map(){|coin| symbol.include? coin}
+    check = order_array.map(){|coin| trade.include? coin}
   rescue
   end
 
   check.any? if check
 end
 
-TRADE3_SET = TRADE2_SET.map do |trade|
-  Parallel.map(trade,in_threads:trade.values.length) do |orders|
-    result = orders.map do |order|
-        if order
-          puts "new order"
-          puts order
-          if order.is_a? Array
-            order.map do |item|
-              # puts "new item"
-              # puts item
-              trade if search_for_partial_matches(item['symbol'],RESULTING_CURRENCY_PAIRS)
-            end
-          elsif order.is_a? Hash
-            # puts "new item"
-            # puts "order length is 1"
-            # puts order
-            trade if search_for_partial_matches(order['symbol'],RESULTING_CURRENCY_PAIRS)
-          end
-        end
-      end
-    result.compact.flatten(0)
+def get_matching(trade,order_set,resulting_currency_pairs)
+  resulting_currency_pairs.map do |pair|
+    if trade['symbol'].include? pair
+      order_set.select{|order| order['symbol'] == "#{pair}#{RESULTING_CURRENCY}"}
+    end
   end
 end
 
-puts TRADE3_SET
+TRADE3_SET = RESULTING_CURRENCY_ORDERS
+
+def calculate_result(trade1, trade2, trade3)
+  product = trade1['askPrice'].to_f * trade2['askPrice'].to_f * trade3['askPrice'].to_f
+  product / trade1['askPrice'].to_f
+end
+
+results = Parallel.map(TRADE2_SET,in_threads:TRADE2_SET.length) do |trade|
+    trade.values.map do |trade2set|
+      trade2set.map do |trade2|
+        begin
+        trade_one_and_three = get_matching(trade2,RESULTING_CURRENCY_ORDERS,RESULTING_CURRENCY_PAIRS).compact
+        trade1 = trade_one_and_three[1][0]
+        trade3 = trade_one_and_three[0][0]
+        # puts "trade 1"
+        # puts trade1
+        # puts "trade2"
+        # puts trade2
+        # puts "trade3"
+        # puts trade3
+        # puts trade1.is_a? Array
+        # puts trade2.is_a? Array
+        # puts trade3.is_a? Array
+
+        {:trade1 => trade1['symbol'],:trade2 => trade2['symbol'],:trade3 => trade3['symbol'],:ask1 => trade1['askPrice'],:ask2 => trade2['askPrice'],:ask3 => trade3['askPrice'],:product => calculate_result(trade1, trade2, trade3) }
+        rescue
+          nil
+        end
+      end
+    end
+  end
+
+results = results.flatten(3).compact.select(){|chain| chain[:product].is_a?(Float) && !chain[:product].nan? }
+results = results.sort_by(){|result| result[:product]}
+puts results
